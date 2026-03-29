20214ba078fe4fd526264de8984c87a69fd68b05
Harshit Shrivastava <harshitditto04@gmail.com>
2026-03-30 05:08:04 +0530

    fix: migrate text_splitter to new langchain_text_splitters package to resolve Docker import errors

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
